2f57a5e72941957900205b014a9159e15d62d098
Fernando Rodríguez-Izquierdo Serrano <fernando@rodriguezizquierdo.com>
2024-09-23 07:53:43 +0200

    [PRICE_SCH] feat: initial commit

---
+++ src/test/java/com/ecommerce/price/scheduler/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.price.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
